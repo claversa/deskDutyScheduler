56c94dbcc22b28598d5311ad2385c0fb4cb09a1b
claversa <claversa@stevens.edu>
2024-08-09 19:01:56 -0400

    Please be right

---
+++ RaSched/src/components/Form.tsx
@@ -0,0 +1,163 @@
+import { Dropbox } from 'dropbox';
+import React, { useEffect, useState } from 'react';
+import Papa from 'papaparse';
+
+const ACCESS_TOKEN = 'ACCESS TOKEN HERE'
+
+interface Unavailability {
+    [day: string]: number[]; // object where key is day and value is an array of numbers
+}
+
+interface Interval {
+    start: number;
+    end: number
+}
+
+
+
+
+// hours will be 9-20
+const Form = () => {
+
+    const [unavailability, setUnavailability] = useState<Unavailability>({
+        Monday: [],
+        Tuesday: [],
+        Wednesday: [],
+        Thursday: [],
+        Friday: [],
+    })
+
+    const [intervals, setIntervals] = useState<{ [day: string]: Interval[] }>({
+        Monday: [],
+        Tuesday: [],
+        Wednesday: [],
+        Thursday: [],
+        Friday: [],
+    });
+    const [userName, setUserName] = useState<string>('');
+
+    const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUserName(e.target.value);
+    };
+
+    const handleIntervalChange = (day: string, index: number, field: "start" | "end", value: number) => {
+        const newIntervals = { ...intervals };
+        console.log(newIntervals);
+        newIntervals[day][index][field] = value;
+        setIntervals(newIntervals);
+    }
+
+    const addInterval = (day: string) => {
+        const newIntervals = { ...intervals };
+        newIntervals[day].push({ start: 9, end: 10 });
+        setIntervals(newIntervals);
+    }
+    const removeInterval = (day: string, index: number) => {
+        const currIntervals = { ...intervals };
+        currIntervals[day].splice(index, 1); // replace elem at index 1 with "" nothing
+        setIntervals(currIntervals);
+    }
+
+    useEffect(() => {
+
+        const newUnavailability: Unavailability = {
+            Monday: [],
+            Tuesday: [],
+            Wednesday: [],
+            Thursday: [],
+            Friday: [],
+        }
+        Object.keys(intervals).forEach((day) => {
+            intervals[day].forEach((interval) => {
+                const { start, end } = interval; // destructure interval
+                for (let i = start; i < end; i++) {
+                    newUnavailability[day].push(i);
+                }
+                console.log(newUnavailability);
+
+
+            })
+        })
+        setUnavailability(newUnavailability);
+
+    }, [intervals]);
+
+    const convertToCSV = () => {
+        const csvdata = [];
+
+        // const headerRow = ['Name', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+        // csvdata.push(headerRow);
+
+        const dataRow = []
+        dataRow.push(unavailability.Monday,
+            unavailability.Tuesday,
+            unavailability.Wednesday,
+            unavailability.Thursday,
+            unavailability.Friday);
+        csvdata.push(dataRow);
+
+        const csvStr = Papa.unparse(csvdata);
+        return csvStr;
+    }
+
+    const dropbox = new Dropbox({ accessToken: ACCESS_TOKEN })
+    const uploadCSV = async (csvStr: string) => {
+        try {
+            const response = await dropbox.filesUpload({
+                path: `/${userName}-unavailable.csv`,
+                contents: csvStr,
+                mode: { '.tag': 'add' }
+            });
+
+            console.log('File uploaded successfully:', response);
+        } catch (error) {
+            console.error('Error uploading file:', error);
+        }
+    }
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        const csvStr = convertToCSV();
+        // Handle form submission here, e.g., send `unavailability` to your backend
+        await uploadCSV(csvStr);
+        console.log(csvStr);
+        console.log("Submission complete");
+    };
+
+
+
+
+
+
+    return (
+        // <div className=' rounded-lg w-5/6 bg-slate-100 mx-auto'>
+        <form className='py-9 px-4 mx-auto' onSubmit={handleSubmit}>
+            <input className='hover:shadow-md shadow-lg py-2 px-3 mb-10 rounded-lg' type="text" value={userName} onChange={handleNameChange} placeholder='Name' required />
+            <h3 className='font-lg text-red-800 font-bold mb-4'>Please indicate your UNavailability in intervals</h3>
+            <div className='flex justify-center flex-row gap-4'>
+                {Object.keys(intervals).map((day) =>
+                    <div className='w-48 flex-shrink-0 rounded-lg bg-slate-100 px-2 py-2' key={day}>
+                        <h3 className='font-lg text-red-800 font-bold'>{day}</h3>
+                        {intervals[day].map((interval, index) => (
+                            <div key={index} className='mb-4'>
+                                <div className='flex flex-row justify-center'>
+                                    <input className='w-12 rounded-lg shadow-lg text-center' type="number" value={interval.start} onChange={(e) => handleIntervalChange(day, index, 'start', parseInt(e.target.value))} min={9} max={19} placeholder="start" required />
+                                    <p>-</p>
+                                    <input className='w-12 rounded-lg shadow-lg text-center' type="number" value={interval.end} onChange={(e) => handleIntervalChange(day, index, 'end', parseInt(e.target.value))} min={interval.start + 1} max={20} placeholder='end' required />
+                                    <button className='hover:bg-red-500 ml-4 shadow-lg rounded-lg text-white text-xs px-2 py-1 bg-red-600' onClick={() => removeInterval(day, index)}>Remove</button>
+                                </div>
+                            </div>
+                        ))}
+                        <button className='hover:bg-blue-400 rounded-lg bg-blue-500 text-white text-xs py-1 px-2' type="button" onClick={() => addInterval(day)}>
+                            + Add Interval
+                        </button>
+                    </div>)}
+            </div>
+            <button className='px-4 py-2 hover:bg-green-500 bg-green-600 text-white rounded-lg mt-7' type="submit">Submit</button>
+        </form>
+        // </div>
+
+    )
+}
+
+export default Form;
---
+++ optimizer.py
@@ -0,0 +1,123 @@
+from ortools.sat.python import cp_model
+# from typing import Union
+import csv
+import os
+from collections import defaultdict
+
+# directory to loop thru
+directory = r"C:\Users\clave\OneDrive\Desktop\Scheduler_app_resources\ra_schedules"
+
+num_ras = 3
+num_days = 5  # m-f
+num_shifts = 11  # 9-8  == 11 shifts
+
+all_ras = list(range(num_ras))
+all_days = list(range(num_days))
+all_shifts = list(range(num_shifts))
+
+# all_shifts_uncorrected = []
+# for i in range(9, 21):
+#     all_shifts_uncorrected.append(i)
+
+# # 1 means unavailable maybe alphabetize so first in list is amber for ex:
+# # [ [11 bits for M], [11 bits for T], [11 bits for W], [11 bits for R], [11 bits for F] ], <-- this is Amber
+# # [ [11 bits for M], [11 bits for T], [11 bits for W], [11 bits for R], [11 bits for F] ], <-- this is Caroline
+# # [ [11 bits for M], [11 bits for T], [11 bits for W], [11 bits for R], [11 bits for F] ], <-- this is Dean
+# unavailabilityMatrix = []
+
+ra_list = []
+# a big array, where each ra has an array and their array has 5 arrays for the days, filled with 11 0s for the shifts (means fully available)
+unavailability_matrix = [
+    [[0 for _ in range(num_shifts)] for _ in range(num_days)] for _ in range(num_ras)]
+
+# Iterate over files in directory
+for name in os.listdir(directory):
+    # Open file
+    # creates path for every filename by appending it
+    with open(os.path.join(directory, name)) as f:
+        ra_name = name.split('-')[0]
+        ra_list.append(ra_name)  # ra name list, also gives us their index
+        ra_spot_in_matrix = ra_list.index(ra_name)
+        csvFile = csv.reader(f)
+        for line in csvFile:
+            for dayNum in range(len(line)):
+                # each day only has the shifts that don't work
+                for unavail in line[dayNum].split(','):
+                    if (unavail):
+                        intUnavail = (int(unavail))
+                        # change 0 to 1 to show unavailable, subtract 9 to account for starting at 9
+                        unavailability_matrix[ra_spot_in_matrix][dayNum][intUnavail-9] = 1
+
+# NOW unavailability matrix is full
+
+
+model = cp_model.CpModel()
+
+# ADDING METHODS are for ADDING CONSTRAINTS TO THE MODEL!!!
+
+shifts = {}
+for r in all_ras:
+    for d in all_days:
+        for s in all_shifts:
+            shifts[(r, d, s)] = model.new_bool_var(f"shift_ra{r}_d{d}_s{s}")
+            # shifts[(r, d, s)] equals 1 if shift s is assigned to ra r on day d, and 0 otherwise
+
+# constraints
+# 1 ra per shift
+for d in all_days:
+    for s in all_shifts:
+        # CONSTRAINT 1:
+        # for each shift, the sum of the ras assigned to that shift is 1
+        model.add_exactly_one(shifts[(r, d, s)] for r in all_ras)
+
+for ra in all_ras:
+    for shift in all_shifts:
+        for day in all_days:
+            if unavailability_matrix[ra][day][shift]:
+                # CONSTRAINT 2: if ra is unavailable that day, do not schedule them
+                model.Add(shifts[ra, day, shift] == 0)
+
+min_shifts_per_ra = (num_days * num_shifts) // num_ras
+if num_days * num_shifts % num_ras == 0:
+    max_shifts_per_ra = min_shifts_per_ra  # all have equal # shifts
+else:
+    # can have at most 1 extra for some ras
+    max_shifts_per_ra = min_shifts_per_ra+1
+
+# print(max_shifts_per_ra)
+# print(min_shifts_per_ra)
+
+# Constraints: Each RA's total shifts must be between min_shifts_per_ra and max_shifts_per_ra
+for ra in range(num_ras):
+    shifts_worked = sum(shifts[(ra, day, shift)] for day in range(
+        num_days) for shift in range(num_shifts))
+    model.Add(shifts_worked >= min_shifts_per_ra)  # Minimum shifts
+    model.Add(shifts_worked <= max_shifts_per_ra)  # Maximum shifts
+
+
+solver = cp_model.CpSolver()
+status = solver.Solve(model)
+
+csv_data = [[''] * num_days for _ in all_shifts]
+day_headers = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+for d in all_days:
+    csv_data.append([day_headers[d]] + [''] * num_shifts)
+
+if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
+    for day in all_days:
+        for shift in all_shifts:
+            ra_assigned = ''
+            for ra in all_ras:
+                if solver.Value(shifts[(ra, day, shift)]):
+                    ra_assigned = f"RA {ra_list[ra]}"  # gets actual name
+                    break
+            csv_data[shift][day] = ra_assigned
+else:
+    print("no solution")
+
+with open("desk_schedule.csv", 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['Shift'] + day_headers)
+    for shift in range(num_shifts):
+        writer.writerow(
+            [f'Time: {(shift+9) % 12 if (shift+9) % 12 != 0 else 12} '] + csv_data[shift])
